202d7d7ebf39171bd59037736474e08e22fa4b0a
Christophe MEJEAN <christophemma@live.fr>
2020-11-15 20:43:32 +0100

    Part 6 - Body Velocity: A world of physics

--- src/game/firstgame.js
+++ src/game/firstgame.js
@@ -78,6 +78,9 @@ function create() {
         frameRate: 10,
         repeat: -1
     });
+
+    player.body.setGravityY(300);
+    this.physics.add.collider(player, platforms);
 }
 
 function update() {}
